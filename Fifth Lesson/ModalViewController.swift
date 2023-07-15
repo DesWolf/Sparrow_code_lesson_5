3716b8b4c651888373fedbaf8997d05fac3bb284
MaxOkuneev <okuneev@trend.tech>
2023-07-15 13:32:07 +0300

    main

---
+++ Fifth Lesson/ModalViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ModalViewController.swift
+//  Fifth Lesson
+//
+//  Created by Максим Окунеев on 14.07.2023.
+//
+
+import UIKit
+
+protocol ModalViewControllerDelegate {
+    func segmentChanged(value: Int)
+}
+
+class ModalViewController: UIViewController {
+    var modalDelegate: ModalViewControllerDelegate?
+    
+    lazy var segmentControl: UISegmentedControl = {
+        let segmentControl = UISegmentedControl (items: ["280px","150px"])
+        
+        segmentControl.selectedSegmentIndex = 0
+        segmentControl.frame = CGRect(x: 35, y: 200, width: 250, height: 50)
+        segmentControl.addTarget(self, action: #selector(segmentAction(_:)), for: .valueChanged)
+    
+        return segmentControl
+    }()
+    
+    lazy var closeButton: UIButton = {
+        var button = UIButton(type: .close)
+        button.setImage(.init(systemName: "xmark"), for: .normal)
+        button.addTarget(self, action: #selector(closeButtonTap), for: .touchUpInside)
+        
+        return button
+    }()
+    
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+    }
+    
+    func setupView() {
+        self.view.addSubview(segmentControl)
+        self.view.addSubview(closeButton)
+        
+        segmentControl.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            segmentControl.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            segmentControl.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            closeButton.centerYAnchor.constraint(equalTo: segmentControl.centerYAnchor, constant: 0),
+            closeButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10),
+            closeButton.widthAnchor.constraint(equalToConstant: 30),
+            closeButton.heightAnchor.constraint(equalToConstant: 30)
+            
+        ])
+    }
+    
+    @objc func segmentAction(_ segmentedControl: UISegmentedControl) {
+           switch (segmentedControl.selectedSegmentIndex) {
+           case 0:
+               modalDelegate?.segmentChanged(value: 0)
+           case 1:
+               modalDelegate?.segmentChanged(value: 1)
+           default:
+               break
+           }
+       }
+    
+    @objc func closeButtonTap() {
+        self.dismiss(animated: true)
+    }
+}
--- Fifth Lesson/ViewController.swift
+++ Fifth Lesson/ViewController.swift
@@ -7,13 +7,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//- Контроллер открывается не на весь экран. Ширина всегда 300pt.
+//- По умолчанию выбран первый пункт с высотой 280pt. Если выбрать второй пункт - высота станет 150pt. Высота меняется анимировано.
+//- Кнопка закрыть в правом верхнем углу закрывает контроллер.
+//- Контроллер сверху привязан к центру кнопки треугольничком (см. видео).
 
+class ViewController: UIViewController {
+    
+    lazy var presentButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Present", for: .normal)
+        button.addTarget(self, action: #selector(presentButtonTap), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    lazy var modalVC: ModalViewController = {
+        let modalVC = ModalViewController()
+        modalVC.modalPresentationStyle = .popover
+        modalVC.preferredContentSize = CGSize(width: 300, height: 280)
+        modalVC.modalDelegate = self
+        
+        return modalVC
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
+    
+    func setupView() {
+        self.view.addSubview(presentButton)
+        presentButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            presentButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            presentButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    @objc func presentButtonTap() {
+        setupPopover()
+        self.present(modalVC, animated: true, completion: nil)
+    }
+    
+    func setupPopover() {
+        let rect = CGRect(
+            origin: CGPoint(x: self.view.frame.width / 2, y: self.presentButton.frame.maxY),
+            size: CGSize(width: 1, height: 1)
+        )
+        let popOver = modalVC.popoverPresentationController
+        
+        popOver?.delegate = self
+        popOver?.permittedArrowDirections = .up
+        popOver?.sourceView = self.view
+        
+        popOver?.sourceRect = rect
+    }
+    
+    func changePopOverHeight(segment: Int) {
+        switch segment {
+        case 0:
+            modalVC.preferredContentSize = CGSize(width: 300, height: 280)
+        default:
+            modalVC.preferredContentSize = CGSize(width: 300, height: 150)
+        }
+    }
+}
 
+extension ViewController: UIPopoverPresentationControllerDelegate {
+    func adaptivePresentationStyle(
+    for controller: UIPresentationController,
+        traitCollection: UITraitCollection)
+        -> UIModalPresentationStyle {
+            return .none
+    }
 
 }
 
+extension ViewController: ModalViewControllerDelegate {
+    func segmentChanged(value: Int) {
+        changePopOverHeight(segment: value)
+    }
+}
